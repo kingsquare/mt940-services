78fac419c82d0970f8a6d96c6527a5296ce41911
Robin Speekenbrink <robin@kingsquare.nl>
2015-03-13 16:17:24 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Kingsquare PHP MT940 service
+
+This is a simple service for quickly testing out by parsing Mt940 files :)
---
+++ web/index.php
@@ -0,0 +1,22 @@
+<?php
+date_default_timezone_set("UTC");
+require dirname(__DIR__).'/vendor/autoload.php';
+$knownBanks = array('detect / unkown', 'Rabo', 'Ing', 'Abn', 'Spk');
+
+// simple example stuff
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['mt940'])) {
+    // simple handling
+    $chosenBank = (int) $_POST['bank'] ?: 0;
+    if (empty($knownBanks[$chosenBank])) {
+        $chosenBank = 0;
+    }
+    $class = '\Kingsquare\Parser\Banking\Mt940\Engine\\'.$knownBanks[$chosenBank];
+    $parser = new \Kingsquare\Parser\Banking\Mt940();
+    $parsedStatements = $parser->parse((string) $_POST['mt940']);
+
+    if (!headers_sent()) {
+        header('Content-type: application/json');
+    }
+    die(json_encode($parsedStatements, JSON_PRETTY_PRINT));
+}
+readfile(__DIR__.'/form.html');
